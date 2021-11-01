63fa8d920e61337659b9ab3c97ea2566fbca004c
Franny Medico <m.francisca.medico@gmail.com>
2021-11-01 17:46:52 +0100

    added rootReducer and tasksReducer

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import rootReducer from "../reducers/rootReducer";
 
 const configureStore = (initialValues) => {
   return createStore(
